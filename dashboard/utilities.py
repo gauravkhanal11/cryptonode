44fceff8f6f447ef2a0b502430c2d88a1cda6354
gauravkhanal11 <gauravkhanal26@gmail.com>
2025-12-29 23:47:52 -0500

    Addition of new cards

---
+++ dashboard/utilities.py
@@ -0,0 +1,4 @@
+from num2words import num2words
+
+def num_to_word(amount):
+    return num2words(amount).capitalize()
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
-from datetime import datetime, timezone
+from .utilities import num_to_word
 import os
-
-def epoch_date(epoch):
-    return datetime.fromtimestamp(epoch, tz=timezone.utc)
 
 market_endpoint="https://api.coinpaprika.com/v1/global"
 data = requests.get(url=market_endpoint)
@@ -32,12 +29,12 @@ def dashboard(request):
     ]
     filtered_data.sort(key=lambda x: x['rank'])
     sliced = filtered_data[:20]
-    top_ten_ids= [coin.get('id') for coin in sliced] 
+    top_ids= [coin.get('id') for coin in sliced] 
     #return JsonResponse(top_ten_ids,safe=False)
 
     each_coin = []
     api_endpoint_ind_coins = "https://api.coinpaprika.com/v1/tickers/{id}"
-    for coin_id in top_ten_ids:
+    for coin_id in top_ids:
         try:
             url = api_endpoint_ind_coins.format(id=coin_id)
             coin_details = requests.get(url=url)
@@ -53,6 +50,7 @@ def dashboard(request):
         "symbol": card.get('symbol'),
         "rank": card.get('rank'),
         "trading_price": card.get('quotes').get('USD').get('price'),
+        "market_cap_amount": card.get('quotes',{}).get('USD',{}).get('market_cap',{}),
         "market_cap_percent": round((card.get('quotes',{}).get('USD',{}).get('market_cap',{}) / crypto_market_cap) * 100,2),
         "charts": {
             "fifteen_min": card.get('quotes',{}).get('USD',{}).get('percent_change_15m',0),
@@ -66,6 +64,25 @@ def dashboard(request):
     for card in each_coin
     ]
     #return JsonResponse(card_detail,safe=False)
+
+# ================================================= PIE CHART =======================================================
+    pie_chart_raw_data= card_detail[:5]
+    pie_chart = [
+        {
+            "name": data.get('name'),
+            "market_cap_percent": data.get('market_cap_percent'),
+            "market_cap_amount": data.get('market_cap_amount')
+        }
+    for data in pie_chart_raw_data
+    ]
+    pie_chart.append(
+        {
+            "name": "Others",
+            "market_cap_percent": round(100 - sum(record['market_cap_percent'] for record in pie_chart),2),
+            "market_cap_amount": crypto_market_cap - sum(record['market_cap_amount'] for record in pie_chart)
+        }
+    )
+    #return JsonResponse(pie_chart,safe=False)
 
 #===================================================MARKET CAP DETAILS==============================================
 
@@ -81,7 +98,11 @@ def dashboard(request):
     "today_movement": market_data_json.get('volume_24h_usd'),
     "last_updated": market_data_json.get('last_updated'),
     "ath_market_cap": market_data_json.get('market_cap_ath_value'), #Bar 2
-    'ath_market_cap_date': market_data_json.get('market_cap_ath_date')
+    "ath_market_cap_date": market_data_json.get('market_cap_ath_date').split("T")[0],
+    "today_market_cap_amount_word": num_to_word(crypto_market_cap),
+    "ath_market_cap_amount_word": num_to_word(market_data_json.get('market_cap_ath_value')),
+    "total_cryptos": market_data_json.get('cryptocurrencies_number'),
+    "market_cap_change_24h": market_data_json.get('market_cap_change_24h')
     }
     #return JsonResponse(filtered_market_info,safe=False)
 
@@ -109,10 +130,12 @@ def dashboard(request):
     ]
     news_json_filtered.sort(key=lambda x: x['published_at'], reverse=True)
     #return JsonResponse(news_json_filtered,safe=False)
-
+ 
+#================================================== FINAL PAGE RENDERING ======================================================
     context = {
         'coins': card_detail,
         'market': filtered_market_info,
+        'pie_chart': pie_chart,
         'news_board': news_json_filtered
     }
     return render(request,'dashboard/coin_list.html',context)
