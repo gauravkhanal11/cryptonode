da145fdc21231ccb1ce5506d14a14d2198a34106
gauravkhanal11 <gauravkhanal26@gmail.com>
2026-01-07 15:43:56 -0500

    Changes to make the website live

--- cryptoworld/settings.py
+++ cryptoworld/settings.py
@@ -27,9 +27,9 @@ if not NEWS_API_KEY:
     raise RuntimeError("News API Key missing")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] #
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
